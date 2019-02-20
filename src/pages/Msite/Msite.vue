700f43043020672b9545a49e175b73ad38357fd0
hanghu <hanghu@hengtiansoft.com>
2019-02-20 15:25:43 +0800

    swiper组件实现首页分类轮播

--- src/pages/Msite/Msite.vue
+++ src/pages/Msite/Msite.vue
@@ -311,8 +311,21 @@
 
 <script>
   import HeaderTop from '../../components/HeaderTop/HeaderTop'
+  import Swiper from 'swiper'
+  import 'swiper/dist/css/swiper.min.css'
+
 
   export default {
+    mounted(){
+      new Swiper('.swiper-container', {
+        loop: true, // 循环模式选项
+
+        // 如果需要分页器
+        pagination: {
+          el: '.swiper-pagination',
+        }
+      })
+    },
     components: {
       HeaderTop
     }
