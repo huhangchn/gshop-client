6380a28e98022f35c0279508957d1212bd75b67a
hanghu <hanghu@hengtiansoft.com>
2019-02-20 16:07:56 +0800

    登陆注册静态页面，底部导航动态显示隐藏

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Msite from '../pages/Msite/Msite'
 import Search from '../pages/Search/Search'
 import Order from '../pages/Order/Order'
 import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
 
 Vue.use(VueRouter)
 
@@ -16,19 +17,35 @@ export default new VueRouter({
     },
     {
       path: '/msite',
-      component: Msite
+      component: Msite,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/search',
-      component: Search
+      component: Search,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/order',
-      component: Order
+      component: Order,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/profile',
-      component: Profile
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
